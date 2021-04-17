83d2c40099e64e5db30776897f5fc6c2ca898c70
Thiago Vivan <thiago-vivanfsa@hotmail.com>
2021-04-17 20:09:02 -0300

    classe Convidado modificada

--- src/tp03/Convidado.java
+++ src/tp03/Convidado.java
@@ -1,44 +1,30 @@
 package tp03;
 
-import java.util.Random ;                                        /* utilizado para randomizar se o convidado está confirmado ou não */
-import java.util.Scanner;
-
+                                       
 public class Convidado extends Pessoa {
   
-	private boolean confirmado;;                                /* convidados */
+	private boolean confirmado;;                                
 	             
 		
 	
-	public String  regConvidado() {
-		Convidado cv = new Convidado();
-		
-		Scanner ler = new Scanner(System.in);
-		
-		System.out.println("informe o nome do convidado");
-		
-		cv.nome  = ler.next();
-		
-		System.out.println("informe o numero de telefone do convidado");
-		
-		Scanner lerr = new Scanner(System.in);
-		
-		cv.telefone = lerr.nextInt();
-		
-		return nome;
-		
-		
+	public Convidado(String nome, int telefone, boolean confirmado) {
+		super(nome, telefone);
+		this.confirmado = confirmado;
+	
 	}
-	
-	
-	
-	
+
+
+
 	public boolean isConfirmado() {
 		return confirmado;
 	}
+
+
+
 	public void setConfirmado(boolean confirmado) {
 		this.confirmado = confirmado;
 	}
+		
 	
-	/* classe de convidados- confirmados-nome */
-}
- 
+}	
+	
