60ecb6cba51fe9103ecd399d61d4a229b5e99c72
Thiago Vivan <thiago-vivanfsa@hotmail.com>
2021-04-17 20:08:11 -0300

    Classe principal comentada

--- src/controle/Principal.java
+++ src/controle/Principal.java
@@ -1,11 +1,46 @@
-package controle;
+ package controle;
 
-import visao.*;
+import javax.swing.JOptionPane;
 import tp03.*;
-
 
 public class Principal {
 	
+
+	private Usuario u;
+	
+//	Funçao para cadastrar Usuário
+//	
+//	
+	public void cadastraUsu(String l, String s, String n, int t) {
+		
+		u = new Usuario(l,s,n,t);
+		
+		JOptionPane.showMessageDialog(null, "Sua conta foi criada com sucesso!");
+		
+	
+	}
+	
+
+// Função para efetuar login de usuario já cadastrado	
+//	
+//	
+	public void realizarLogin(String l, String s) {
+
+	
+		if(l.equalsIgnoreCase(u.getLogin())) {
+				if(s.equals(u.getSenha())){
+					
+					JOptionPane.showMessageDialog(null, "Bem vindo " + u.getNome());
+					;
+					
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "A senha inserida está incorreta", null, JOptionPane.ERROR_MESSAGE);
+					
+				}
+			
+		}
+	}		
 
 
 
