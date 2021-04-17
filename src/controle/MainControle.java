5823bafc708c959ccb6069d0cc105534f4d6bbfa
Thiago Vivan <thiago-vivanfsa@hotmail.com>
2021-04-17 20:07:21 -0300

    Classe main do programa

--- src/controle/MainControle.java
+++ src/controle/MainControle.java
@@ -8,14 +8,16 @@ public class MainControle {
 
 		public static Inicial ini = new Inicial();
 		
+
 		
+//		Funçao main do programa, gera a Janela inicial
+//		
+//		
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 
 			
 			ini.janelaInicial();
-
-
 	
 			
 	}
